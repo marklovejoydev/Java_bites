0c70540ff68ee12a2d25f24ac15a532c06d9eabf
marklovejoy <marklovejoydev@gmail.com>
2024-05-24 12:06:52 +0100

    inital java commit

---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,14 @@
+package bites.examples;
+
+public class JavaQuiz {
+    public static void main(String[] args){
+        System.out.println("Q Does all Java code have to be inside a class?");
+        System.out.println("A Yes");
+        System.out.println("Q What is compilation?");
+        System.out.println("A process by which java code is converted to byte code prior to execution");
+        System.out.println("Q What is special about the main method?");
+        System.out.println("A It is executed automatically and you need one per program");
+        System.out.println("Q What does void mean?");
+        System.out.println("A In the context of a method signature, it means the method is not intended to return anything");
+    }
+}
